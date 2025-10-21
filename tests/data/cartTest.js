d315fffbcf982d915bd229b6a0d0d315dd747943
Lewis-mbui <lewmugo14479@gmail.com>
2025-10-21 13:17:14 +0300

    add updateDeliverOption test suite to cartTest.js

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage, removeFromCart } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart, updateCartQuantity, updateDeliveryOption } from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
   beforeEach(() => {
@@ -86,4 +86,33 @@ describe('test suite: removeFromCart', () => {
 
     expect(cart.length).toEqual(2);
   });
+});
+
+describe('test suite: updateDeliveryOption', () => {
+  beforeEach(() => {
+    spyOn(Storage.prototype, 'setItem');
+
+    spyOn(Storage.prototype ,'getItem').and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+          quantity: 2,
+          deliveryOptionId: '1'
+        }, {
+          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+          quantity: 1,
+          deliveryOptionId: '2'
+        }
+      ]);
+    });
+
+    loadFromStorage();
+  });
+
+  it('updates the delivery option for a product', () => {
+    updateDeliveryOption('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', '3');
+
+    expect(cart.length).toEqual(2);
+    expect(cart[0].deliveryOptionId).toEqual('3');
+  })
 });
