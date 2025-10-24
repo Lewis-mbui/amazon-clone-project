c973fbcc3d95fa4c2f71968d095bd5769d1776ff
Lewis-mbui <lewmugo14479@gmail.com>
2025-10-24 08:13:22 +0300

    convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import { renderOrderSummary } from "../scripts/checkout/orderSummary.js";
+import { renderPaymentSummary } from "../scripts/checkout/paymentSummary.js";
 import { formatCurrency } from "../scripts/utils/money.js";
 
 export function getProduct(productId) {
@@ -84,6 +86,34 @@ export class Appliance extends Product {
 }
 
 export let products = [];
+
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if(productDetails.type === 'clothing')
+        return new Clothing(productDetails);
+
+      if (productDetails.type === 'appliance')
+        return new Appliance(productDetails);
+
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });
+
+  return promise;
+}
+
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
 
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
