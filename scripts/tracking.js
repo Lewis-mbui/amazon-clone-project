9398e07b644baed764cc560c589177a53922886c
Lewis-mbui <lewmugo14479@gmail.com>
2025-10-26 07:24:35 +0300

    exercise: make tracking page interactive

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -11,14 +11,24 @@ async function loadPage() {
 }
 
 function renderTrackingSection() {
+  function calculateDeliveryProgress(deliveryTime, orderTime) {
+    const currentTime = new Date();
+    deliveryTime = new Date(deliveryTime);
+    orderTime = new Date(orderTime);
+
+    return Number((((currentTime - orderTime) / (deliveryTime - orderTime)) * 100)
+      .toFixed(2));
+  }
+
   const url = new URL(window.location.href);
   const orderId = url.searchParams.get("orderId");
   const productId = url.searchParams.get("productId");
   const matchingOrder = getOrder(orderId);
   const matchingProduct = getProduct(productId);
 
-  const matchingProductFromOrder = matchingOrder.products.find((product) => {
-    return product.productId === productId;
+  const matchingProductFromOrder = matchingOrder.products
+    .find((product) => {
+      return product.productId === productId;
   });
 
   const {estimatedDeliveryTime, quantity} = matchingProductFromOrder;
@@ -26,8 +36,13 @@ function renderTrackingSection() {
   const deliveryDateString = dayjs(estimatedDeliveryTime).format(
     'dddd, MMMM D'
   );
-  
 
+  const percentageProgress = calculateDeliveryProgress(
+    estimatedDeliveryTime, matchingOrder.orderTime);
+  
+  console.log(matchingOrder);
+
+  
   const html = `
     <a class="back-to-orders-link link-primary" href="orders.html">
       View all orders
@@ -47,13 +62,13 @@ function renderTrackingSection() {
     />
 
     <div class="progress-labels-container">
-      <div class="progress-label">Preparing</div>
-      <div class="progress-label current-status">Shipped</div>
-      <div class="progress-label">Delivered</div>
+      <div class="progress-label js-progress-label-prep">Preparing</div>
+      <div class="current-status progress-label js-progress-label-ship">Shipped</div>
+      <div class="progress-label js-progress-label-del">Delivered</div>
     </div>
 
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+      <div class="progress-bar" style="width: ${percentageProgress}%"></div>
     </div>
   `;
 
@@ -62,4 +77,22 @@ function renderTrackingSection() {
 
   document.querySelector('.js-cart-quantity')
     .innerHTML = calculateCartQuantity();
+
+  updateProgressStatus(percentageProgress);
+
+  function updateProgressStatus(progress) {
+    document.querySelector('.current-status')
+      .classList.remove('current-status');
+
+    if (progress < 50) {
+      document.querySelector('.js-progress-label-prep')
+        .classList.add('current-status');
+    } else if (progress >= 50 && progress < 100) {
+      document.querySelector('.js-progress-label-ship')
+        .classList.add('current-status');
+    } else {
+      document.querySelector('.js-progress-label-del')
+        .classList.add('current-status');
+    }
+  }
 }
